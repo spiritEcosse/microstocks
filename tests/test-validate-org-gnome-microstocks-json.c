4b941993a9c68dc548b3afee1f29b4610794843c
igor <shevchenkcoigor@gmail.com>
2017-10-13 16:46:38 +0300

    fix: undefined_reference_to__main_

---
+++ tests/test-validate-org-gnome-microstocks-json.c
@@ -0,0 +1,39 @@
+/* test-validate-org-gnome-microstocks-json.c
+ *
+ * Copyright (C) 2017 igor <shevchenkcoigor@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdlib.h"
+#include "glib-object.h"
+#include "jsonj-glib/json-glib.h"
+
+#include <glib.h>
+
+gint
+main (gint   argc,
+      gchar *argv[])
+{
+	JsonParser *parser;
+	JsonNode *root;
+	Gerror *error;
+
+	if (argc < 2)
+		{
+			g_print("Usage: test <filename.json\n");
+			return EXIT_FAILURE;
+		}
+	return 0;
+}

165e80b713739ed398fa56bba23c812e2a25c019
igor <shevchenkcoigor@gmail.com>
2017-10-13 15:23:19 +0300

    fix: Application_exit_success_FAIL

--- tests/test-application-exit-success.c
+++ tests/test-application-exit-success.c
@@ -19,9 +19,28 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 #include <locale.h>
+#include <stdlib.h>
 
 static gint    g_argc;
 static gchar **g_argv;
+
+static gboolean
+on_timeout(gpointer data)
+{
+	GtkApplication *app = data;
+	g_application_release(G_APPLICATION(app));
+	return G_SOURCE_REMOVE;
+}
+
+static void
+on_activate(GtkApplication *app)
+{
+	g_timeout_add_full(G_PRIORITY_LOW, 10, on_timeout, g_object_ref(app),
+			   g_object_unref
+			   );
+	g_application_hold(G_APPLICATION(app));
+}
+
 
 static void
 test_application_exist_success (void)
@@ -32,7 +51,9 @@ test_application_exist_success (void)
 	app = gtk_application_new("org.gnome.Microstocks",
 				  G_APPLICATION_FLAGS_NONE
 				  );
+	g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
 	ret = g_application_run(G_APPLICATION(app), g_argc, g_argv);
+	g_assert_cmpint(ret, == , EXIT_SUCCESS);
 }
 
 int
